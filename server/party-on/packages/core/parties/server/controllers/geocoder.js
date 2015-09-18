4dafa38b07ccd7d2eea13aee4b73983e60160d0b
John French <johnfren@indiana.edu>
2015-09-18 19:17:48 -0400

    initial commit for static site

---
+++ server/party-on/packages/core/parties/server/controllers/geocoder.js
@@ -0,0 +1,18 @@
+var geocoderProvider = 'google';
+var httpAdapter = 'https';
+// optionnal 
+var extra = {
+    apiKey: 'AIzaSyDME6wRuiG06ZNGAg03dOeadTogAvOuhfo', // for Mapquest, OpenCage, Google Premier 
+    formatterPatternKey: '%n%s'       // 'gpx', 'string', ... 
+};
+ 
+var geocoder = require('node-geocoder')(geocoderProvider, httpAdapter, extra);
+
+// example
+geocoder.geocode('123 N Jefferson Bloomington Indiana', function(err, res) {
+    console.log(res);
+});
+
+//TODO failover if l2short and l1short do not match model for university
+
+module.exports = geocoder;
