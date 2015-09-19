ba056dbc2f5383664fb88ec3637b0af71833e732
Max McLennan <mpmclenn@indiana.edu>
2015-09-19 00:51:13 -0400

    Renamed project to culturepage, which was kind of hard. Have not substantially altered directory naming.

---
+++ ios/party-on/party-on/culturepage-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  culturepage-Bridging-Header.h
+//  culturepage
+//
+//  Created by Maxwell McLennan on 9/18/15.
+//  Copyright (c) 2015 Maxwell McLennan. All rights reserved.
+//
+
+#ifndef culturepage_culturepage_Bridging_Header_h
+#define culturepage_culturepage_Bridging_Header_h
+
+#endif
